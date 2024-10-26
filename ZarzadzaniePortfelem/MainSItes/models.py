09e2ada126390721f5b55ecd274829f16b09318e
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-10-26 17:59:35 +0200

    praca na modelami oraz viewsami dodanie pliku styles.css

--- ZarzadzaniePortfelem/MainSItes/models.py
+++ ZarzadzaniePortfelem/MainSItes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    pin = models.IntegerField(max_length=8)
+    key = models.FloatField()
+
+class UsersAccount(models.Model):
+    balance = models.FloatField()
+    history = models.CharField()
+    borrowed = models.FloatField()
+    UserKey = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class placeholder(models.Model):
+    pass
