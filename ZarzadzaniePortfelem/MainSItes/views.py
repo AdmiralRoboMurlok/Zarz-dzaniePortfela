f9835ced0345d77a3298e96b8b4ed8fa8e9f69a5
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-11-08 10:17:18 +0100

    dodanie logiki deposita

--- ZarzadzaniePortfelem/MainSItes/views.py
+++ ZarzadzaniePortfelem/MainSItes/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from . models import *
+from django.db.models import F
+from django.contrib import messages
 
 # Create your views here.
 def main(request):
@@ -41,31 +43,44 @@ def login(request):
 
 def logout(request):
     auth.logout(request)
-
     return redirect("main")
 
 @login_required(login_url="login")
 def profile(request):
-    return render(request, 'profile.html')
+    context = {
+        'pk': request.user.pk
+    }
+    return render(request, 'profile.html', context)
 
 @login_required(login_url="login")
 def wallet(request):
     Model_Data = UsersAccount.objects.all()
 
-    return render(request, 'wallet.html', context={
-        "variables":Model_Data
-    })
+    context = {
+        'pk': request.user.pk,
+        'variables': Model_Data
+    }
+
+    return render(request, 'wallet.html', context)
 
 @login_required(login_url="login")
 def deposit(request, pk):
+    context = {
+        'pk': request.user.pk
+    }
+
     if request.method == 'POST':
-        deposit = float(request.POST.get('AddMon'))
-        total = UsersAccount.objects.get(id=pk)
-        total = total + deposit
+        deposit_amount = float(request.POST.get('AddMon'))
+        user_acc = get_object_or_404(UsersAccount, id=pk)
 
-        total.save()
+        user_acc.balance = F('balance') + deposit_amount
+        user_acc.save()
 
-    return render(request, 'deposit.html')
+        user_acc.refresh_from_db()
+        messages.success(request, "Money has been added to your account")
+        return redirect(wallet)
+
+    return render(request, 'deposit.html', context)
 
 @login_required(login_url="login")
 def withdraw(request):
